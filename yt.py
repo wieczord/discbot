e3afe6f2e3a9cf0fcb55af9bc02c61c745a009cd
wieczord <tdw146@gmail.com>
2023-06-29 22:17:24 +0200

    Fix URL source getting for Youtube

--- yt.py
+++ yt.py
@@ -10,5 +10,5 @@ class Youtube(AudioProvider):
     def get_source(self, song: SongName | URL) -> discord.FFmpegPCMAudio:
         with yt_dlp.YoutubeDL(config["YDL_OPTIONS"]) as ydl:
             info = ydl.extract_info(song, download=False)
-            source = info['formats'][0]['url']
+            source = info['url']
         return discord.FFmpegPCMAudio(source)
