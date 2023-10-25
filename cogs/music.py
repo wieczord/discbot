ff0e99701b2869f9f85b6835e7d69dfc2a22224c
wieczord <tdw146@gmail.com>
2023-10-25 18:02:13 +0200

    WIP

--- cogs/music.py
+++ cogs/music.py
@@ -1,28 +1,65 @@
+from typing import Optional
+
 from discord.ext.commands import Cog, command
 
-from player import MusicPlayer, AudioProvider
+from music_queue import MusicQueue
+from player import MusicPlayer, AudioProvider, SourceFetchError, URL, SongName
 
 
 class MusicCog(Cog):
-    def __init__(self, bot, provider: AudioProvider):
+    def __init__(self, bot, provider: AudioProvider, queue: MusicQueue):
         self.bot = bot
         self.provider = provider
-        self.player = MusicPlayer(audio_provider=self.provider)
+        self.player = MusicPlayer(audio_provider=self.provider, queue=queue)
         self.voice_client = None
+        self.channel_id = None
 
     @command()
     async def join(self, ctx):
         channel = ctx.author.voice.channel
         self.voice_client = await channel.connect()
+        self.channel_id = self.voice_client.channel.id
 
     @command()
     async def leave(self, ctx):
         voice_client = ctx.voice_client
         await voice_client.disconnect()
         self.voice_client = None
+        '''
+!play https://www.youtube.com/watch?v=zI383uEwA6Q
+!queue https://www.youtube.com/watch?v=JjbnO5yUpYc
+        '''
 
     @command()
-    async def play(self, ctx, url: str = ""):
+    async def play(self, ctx, song: Optional[SongName | URL] = None):
         if self.voice_client is None:
             await ctx.invoke(self.join)
-        self.player.play(url, self.voice_client)
+        try:
+            await self.player.play(song=song, client=self.voice_client, channel_id=self.channel_id)
+        except SourceFetchError:
+            await ctx.send("No song found")
+
+    @command()
+    async def stop(self, ctx):
+        self.voice_client.stop()
+
+    @command()
+    async def pause(self, ctx):
+        self.voice_client.pause()
+
+    @command()
+    async def resume(self, ctx):
+        self.voice_client.resume()
+
+    @command()
+    async def skip(self, ctx):
+        self.voice_client.stop()
+        await ctx.invoke(self.play)
+
+    @command()
+    async def queue(self, ctx, url: str = ""):
+        try:
+            await self.player.add_to_queue(url=url, channel_id=self.channel_id)  # TODO ID sie beda zgadzac zawsze?
+            await ctx.send(f"Added {url} to queue")
+        except SourceFetchError:
+            await ctx.send(f"Could not add {url} to queue")
--- player.py
+++ player.py
@@ -1,27 +1,51 @@
-from typing import Protocol, TypeAlias
+from typing import Protocol, TypeAlias, Optional
 
 import discord
+
+from music_queue import MusicQueue, StreamableSource
 
 SongName: TypeAlias = str
 URL: TypeAlias = str
 
 
+class SourceFetchError(Exception):
+    ...
+
+
 class AudioProvider(Protocol):
-    def get_source(self, song: SongName | URL) -> discord.FFmpegPCMAudio:
+    def get_source(self, song: SongName | URL) -> Optional[StreamableSource]:
         ...
 
 
 class Player(Protocol):
     audio_provider: AudioProvider
 
-    def play(self, song: SongName | URL, channel: discord.VoiceChannel) -> None:
+    async def play(self, song: SongName | URL, channel: discord.VoiceChannel) -> None:
         ...
 
 
 class MusicPlayer(Player):
-    def __init__(self, audio_provider: AudioProvider):
+    def __init__(self, audio_provider: AudioProvider, queue: MusicQueue):
         self.audio_provider = audio_provider
+        self.music_queue = queue
 
-    def play(self, song: SongName | URL, channel: discord.VoiceChannel) -> None:
-        source = self.audio_provider.get_source(song)
-        channel.play(source)
+    async def play(
+        self, song: Optional[SongName | URL], client: discord.VoiceClient, channel_id: int
+    ) -> None:
+        if song is None:
+            source = await self.music_queue.next(channel_id=client.channel.id) #TODO ujednolicic typ tego channelu bo jest syf, wszedzie inaczej
+        else:
+            source = self.audio_provider.get_source(song)
+        if not source:
+            raise SourceFetchError
+
+        client.play(discord.FFmpegPCMAudio(source))
+
+    async def add_to_queue(self, channel_id: int, url: URL) -> None:
+        source = self.audio_provider.get_source(url)
+        if not source:
+            raise SourceFetchError
+        self.music_queue.add(channel_id, source)
+
+
+# TODO TRZYMANIE TEGO W QUEUE JAKO -SOURCE BEDZIE CHUJOWE, lepiej przerzucac te urle i napisac jakis wrapper na discord.FFmpegPCMAudio
