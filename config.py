b693115cfbf3e4a5e75e82eda11a81e35f3c2958
wieczord <tdw146@gmail.com>
2024-10-13 22:00:20 +0200

    Increase audio bitrate

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ config = {
             {
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "aac",
-                "preferredquality": "64",
+                "preferredquality": "128",
             }
         ],
         "noplaylist": True,
